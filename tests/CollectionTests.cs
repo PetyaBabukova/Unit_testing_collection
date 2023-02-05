bbf9044c7cffc8bf514cfcf5772804d6a2ea8f89
Petya Babukova <p_babukova@abv.bg>
2023-02-05 21:43:38 +0200

    Add tests for: - Get By Index - Set By Index (int) - Set By Index (string) - Get By Invalid Index

--- tests/CollectionTests.cs
+++ tests/CollectionTests.cs
@@ -1,6 +1,5 @@
 
 using Collections;
-using System.ComponentModel.DataAnnotations;
 
 namespace tests
 {
@@ -88,7 +87,52 @@ namespace tests
         
         }
 
+        [Test]
+        public void Test_Collection_GetByIndex() {
 
+            var coll = new Collection<int>(5, 6, 7);
+            var item = coll[1];
+
+            Assert.That (item.ToString(), Is.EqualTo("6"));
+                        
+        }
+
+
+
+        [Test]
+        public void Test_Collection_SetByIndex()
+        {
+
+            var coll = new Collection<int>(5, 6, 7);
+            coll[1] = 666;
+
+            Assert.That(coll.ToString(), Is.EqualTo("[5, 666, 7]"), "Test for Add in range: ");
+
+        }
+
+        [Test]
+        public void Test_Collection_GetByInvalidIndex()
+        {
+            //Arrange
+            var coll = new Collection<string>("Petya", "Stefan");
+            
+                       
+            Assert.That(() => { var item = coll[2]; },
+                Throws.InstanceOf<ArgumentOutOfRangeException>());
+
+        }
+
+
+        [Test]
+        public void Test_Collection_SetByIndex_String() {
+
+            var coll = new Collection<string>("Petya", "Stefan");
+            coll[1] = "Poly";
+
+            Assert.That(coll.ToString(), Is.EqualTo("[Petya2, Poly]"), "Check for Set By Index for String");
+
+
+        }
 
 
 
